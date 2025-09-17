0cb2031a1ef9e52dccc49285ee9549a360c540f3
dylanfalzone1 <dylanfalzone31@gmail.com>
2025-09-17 17:58:19 -0400

    non-participant exp reduced to 1/4 from 1/3 for exp share

--- src/battle_script_commands.c
+++ src/battle_script_commands.c
@@ -61,7 +61,7 @@
 // Participant share     = normalShare * EXPALL_PARTICIPANT_NUM / EXPALL_PARTICIPANT_DEN
 #ifndef EXPALL_TUNING
 #define EXPALL_SHARE_NUM          1
-#define EXPALL_SHARE_DEN          3
+#define EXPALL_SHARE_DEN          4
 #define EXPALL_PARTICIPANT_NUM    2
 #define EXPALL_PARTICIPANT_DEN    3
 #endif
